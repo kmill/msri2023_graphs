201dcaba6c9eb5a8c20f2a690239bfc391acdf87
Kyle Miller <kmill31415@gmail.com>
2023-06-12 21:44:09 +0000

    menger file

---
+++ GraphProjects/Menger.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2023 by authors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Swaroop Hegde, Sung-Yi Liao, Kyle Miller, Jake Weber, Jack Wesley
+-/
+
+import Mathlib.Combinatorics.SimpleGraph.Connectivity
+
+/-! # Menger's theorem for simple graphs
+
+Following the proof in [...]
+-/
